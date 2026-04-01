08083cd5877d06efe0a995b43020795d57ff8a96
Alexandre Paillier <alexandre.paillier@ledger.fr>
2026-04-01 15:51:15 +0200

    Removed useless NFT info presence check

--- src/plugins/erc721/erc721_ui.c
+++ src/plugins/erc721/erc721_ui.c
@@ -123,6 +123,10 @@ static void set_transfer_ui(ethQueryContractUI_t *msg, erc721_context_t *context
 void handle_query_contract_ui_721(ethQueryContractUI_t *msg) {
     erc721_context_t *context = (erc721_context_t *) msg->pluginContext;
 
+    if (msg->item1 == NULL) {
+        msg->result = ETH_PLUGIN_RESULT_ERROR;
+        return;
+    }
     msg->result = ETH_PLUGIN_RESULT_OK;
     switch (context->selectorIndex) {
         case APPROVE:
