6c8aa9be391e552561d3bfe96ac16500baf212df
Alexandre Paillier <alexandre.paillier@ledger.fr>
2026-04-30 16:11:49 +0200

    Change calldata_get_chunk idx type
    (cherry picked from commit 5c102bdb25920c230e6253f432926b3e9c73f915)

--- src/features/generic_tx_parser/calldata.h
+++ src/features/generic_tx_parser/calldata.h
@@ -36,5 +36,5 @@ bool calldata_set_selector(s_calldata *calldata, const uint8_t selector[CALLDATA
 bool calldata_append(s_calldata *calldata, const uint8_t *buffer, size_t size);
 void calldata_delete(s_calldata *node);
 const uint8_t *calldata_get_selector(const s_calldata *calldata);
-const uint8_t *calldata_get_chunk(s_calldata *calldata, int idx);
+const uint8_t *calldata_get_chunk(s_calldata *calldata, uint32_t idx);
 void calldata_dump(const s_calldata *calldata);
