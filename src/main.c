86f0d543f6147c73ac79ac410092c82a439ec1c2
Alexandre Paillier <alexandre.paillier@ledger.fr>
2026-05-27 11:32:46 +0200

    Added a response APDU bounds check when writing SW

--- src/main.c
+++ src/main.c
@@ -116,8 +116,13 @@ uint16_t io_seproxyhal_send_status(uint16_t sw, uint32_t tx, bool reset, bool id
     if (reset) {
         reset_app_context();
     }
+    if (tx > (sizeof(G_io_tx_buffer) - sizeof(sw))) {
+        // writing this SW at this offset would go out of bounds
+        tx = 0;
+        sw = SWO_INSUFFICIENT_MEMORY;
+    }
     U2BE_ENCODE(G_io_tx_buffer, tx, sw);
-    tx += 2;
+    tx += sizeof(sw);
     err = io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, tx);
     if (idle) {
         // Display back the original UX
@@ -357,8 +362,13 @@ void app_main(void) {
         }
 
         // Report Status Word
+        if (tx > (sizeof(G_io_tx_buffer) - sizeof(sw))) {
+            // writing this SW at this offset would go out of bounds
+            tx = 0;
+            sw = SWO_INSUFFICIENT_MEMORY;
+        }
         U2BE_ENCODE(G_io_tx_buffer, tx, sw);
-        tx += 2;
+        tx += sizeof(sw);
 
         // If we are in swap mode and have validated a TX, we send it and immediately quit
         if (quit_now) {
