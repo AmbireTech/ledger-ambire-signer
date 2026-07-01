8e0fa7a6edf1db5b2504567683998acfa504b1f5
Alexandre Paillier <alexandre.paillier@ledger.fr>
2026-07-01 17:25:59 +0200

    Fix fuzzing APDU handlers calls
    Should have been done with 4dafe7ee90b1afed23df5da9176e115a972ade82

--- tests/fuzzing/harness/fuzz_eip7702.c
+++ tests/fuzzing/harness/fuzz_eip7702.c
@@ -6,15 +6,13 @@ int fuzzEIP7702(const uint8_t *data, size_t size) {
     size_t offset = 0;
     size_t len = 0;
     uint8_t p1;
-    unsigned int flags;
 
     while (size - offset > 3) {
         if (data[offset++] == 0) break;
         p1 = data[offset++];
         len = data[offset++];
         if (size - offset < len) return 0;
-        if (handle_sign_eip7702_authorization(p1, data + offset, len, &flags) != SWO_SUCCESS)
-            return 0;
+        if (handle_sign_eip7702_authorization(p1, data + offset, len) != SWO_SUCCESS) return 0;
         offset += len;
     }
     return 0;
--- tests/fuzzing/harness/fuzz_tx_check.c
+++ tests/fuzzing/harness/fuzz_tx_check.c
@@ -1,10 +1,9 @@
 #include "fuzz_utils.h"
 
 int fuzzTxCheck(const uint8_t *data, size_t size) {
-    unsigned int flags;
     if (size < 2) return 0;
 
-    if (handle_tx_simulation(data[0], data[1], data + 2, size - 2, &flags) != SWO_SUCCESS) return 0;
+    if (handle_tx_simulation(data[0], data[1], data + 2, size - 2) != SWO_SUCCESS) return 0;
 
     get_tx_simulation_risk_str();
     get_tx_simulation_category_str();
