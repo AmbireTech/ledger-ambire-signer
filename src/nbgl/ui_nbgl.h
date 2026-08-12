b9ea1ed8fb75b4d53b96d372a325a57b86d6c10f
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2026-08-12 11:38:10 +0200

    Fix multsig glyph

--- src/nbgl/ui_nbgl.h
+++ src/nbgl/ui_nbgl.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "nbgl_use_case.h"
+#include "nbgl_icons.h"
 #include "shared_context.h"
 
 #ifdef SCREEN_SIZE_WALLET
 #define ICON_APP_WARNING  LARGE_WARNING_ICON
 #define ICON_APP_REVIEW   LARGE_REVIEW_ICON
-#define ICON_APP_MULTISIG C_multisig_64px
+#define ICON_APP_MULTISIG LARGE_MULTISIG_ICON
 #if defined(TARGET_APEX)
 #define ICON_LEDGER C_ledger_48px
 #else
